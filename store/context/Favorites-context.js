66aaa3cb879b08c14719aa9d0b9d974bb4761052
thechidubem <kingsleychidubem042@gmail.com>
2022-04-20 17:50:24 +0100

    added redux-toolkit

---
+++ store/context/Favorites-context.js
@@ -0,0 +1,34 @@
+import {useState, createContext} from "react";
+
+export const FavoritesContext = createContext({
+    ids: [],
+    addFavorite: (id) => {},
+    removeFavorite: (id) => {}
+});
+
+function FavoritesContextProvider({children}) {
+   const [favoritesMealIds, setFavoriteMealIds] = useState([]);
+
+   function addFavorite(id) {
+       setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+   };
+
+   function removeFavorite(id) {
+   setFavoriteMealIds((currentFavIds) => 
+   currentFavIds.filter(mealId => mealId !== id)
+   );
+   };
+
+    const value = {
+        ids: favoritesMealIds,
+        addFavorite: addFavorite,
+        removeFavorite: removeFavorite
+    };
+
+    return (<FavoritesContext.Provider value={value}>
+        {children}
+    </FavoritesContext.Provider>
+    );
+};
+
+export default FavoritesContextProvider;
